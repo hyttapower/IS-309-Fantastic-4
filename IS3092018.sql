dea76b88f52e077c22a1c085690f7623465b1987
Sakseid <31402539+Sakseid@users.noreply.github.com>
2018-01-31 15:40:35 +0100

    x

--- IS3092018.sql
+++ IS3092018.sql
@@ -16,7 +16,7 @@ select * from accounts;
 
 drop table address;
 create table address(
-add_id int,
+add_id int auto increment,
 add_mstreet varchar(59) not null,
 add_ostreet varchar(59),
 add_city varchar(59),
@@ -25,4 +25,11 @@ add_pcode int,
 add_location varchar(59),
 add_locdesc varchar (599),
 constraint address_PK primary key(add_id)
+);
+
+create table projects(
+pro_id int auto increment,
+pro_title varchar(59),
+pro_subtitle varchar(59),
+
 );
