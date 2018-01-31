b64fcfe207722e89949a192482abdc13e005cf13
Sakseid <31402539+Sakseid@users.noreply.github.com>
2018-01-31 14:35:22 +0100

    Test

---
+++ IS3092018.sql
@@ -0,0 +1,28 @@
+create database ioby;
+
+drop table accounts;
+create table accounts(
+acc_uname varchar(59),
+acc_pass varchar(59),
+acc_email varchar(59),
+acc_fname varchar(59),
+acc_lname varchar(59),
+acc_type varchar(59), 
+acc_phone int,
+constraint accounts_PK primary key(acc_uname)
+);
+
+select * from accounts;
+
+drop table address;
+create table address(
+add_id int,
+add_mstreet varchar(59) not null,
+add_ostreet varchar(59),
+add_city varchar(59),
+add_state varchar(59),
+add_pcode int,
+add_location varchar(59),
+add_locdesc varchar (599),
+constraint address_PK primary key(add_id)
+);
