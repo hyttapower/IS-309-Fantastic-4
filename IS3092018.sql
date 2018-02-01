794cc4a44d109aec8c4790db9100902577e11298
Sakseid <31402539+Sakseid@users.noreply.github.com>
2018-02-01 10:49:43 +0100

    a

--- IS3092018.sql
+++ IS3092018.sql
@@ -16,7 +16,7 @@ select * from accounts;
 
 drop table address;
 create table address(
-add_id int auto increment,
+add_id int,
 add_mstreet varchar(59) not null,
 add_ostreet varchar(59),
 add_city varchar(59),
@@ -27,9 +27,29 @@ add_locdesc varchar (599),
 constraint address_PK primary key(add_id)
 );
 
+drop table projects;
 create table projects(
-pro_id int auto increment,
+pro_id int,
 pro_title varchar(59),
 pro_subtitle varchar(59),
+pro_description varchar(599),
+pro_crdate date,
+pro_goal int,
+pro_deadline date,
+pro_howdo varchar(59),
+pro_whydo varchar(59),
+pro_focusarea varchar(59),
+pro_status varchar(59),
+pro_type varchar(59),
+pro_heardabout varchar(59),
+constraint projects_PK primary key(pro_id)
+);
 
+select * from projects;
+
+create table association(
+asso_id int,
+asso_title varchar(59),
+asso_url varchar(59),
+constraint association_PK primary key (asso_id)
 );
